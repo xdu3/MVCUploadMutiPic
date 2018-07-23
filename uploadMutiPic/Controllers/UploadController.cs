a76078c7f048e9754789c1bc5d6ae83984299edb
dx3081 <dx3081@gmail.com>
2018-07-24 03:01:46 +0800

    Initial commit

---
+++ uploadMutiPic/Controllers/UploadController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace uploadMutiPic.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private IHostingEnvironment _hostingEnvironment;
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public void OnGet()
+        {
+        }
+
+
+
+        public IActionResult UploadFiles()
+        {
+            return View();
+        }
+
+
+        [HttpPost, ActionName("UploadFiles")]
+        [ValidateAntiForgeryToken]
+        public  IActionResult UploadFilesAction()
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files != null && files.Count > 0)
+                {
+                    string folderName = "Upload";
+                    string webRootPath = _hostingEnvironment.WebRootPath;
+                    string newPath = Path.Combine(webRootPath, folderName);
+                    if (!Directory.Exists(newPath))
+                    {
+                        Directory.CreateDirectory(newPath);
+                    }
+                    foreach (IFormFile item in files)
+                    {
+                        if (item.Length > 0)
+                        {
+                            string fileName = ContentDispositionHeaderValue.Parse(item.ContentDisposition).FileName.Trim('"');
+                            string fullPath = Path.Combine(newPath, fileName);
+                            using (var stream = new FileStream(fullPath, FileMode.Create))
+                            {
+                                item.CopyTo(stream);
+                            }
+                        }
+                    }
+                    return this.Content("Success");
+                }
+                if (files == null)
+                {
+                    return this.Content("null");
+                }
+                if (files.Count == 0)
+                {
+                    return this.Content("count 0");
+                }
+                else
+                {
+                    return this.Content("Fail");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ uploadMutiPic/Models/FileModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace uploadMutiPic.Models
+{
+    public class FileModel
+    {
+        [Required(ErrorMessage ="Please select file.")]
+        [Display(Name ="Browse File")]
+        public IFormFile files { get; set; }
+    }
+}
